976d2652940ee592dead4b8eff2716426030bab1
Alexandre de Medeiros Ribeiro <alexmr@gmail.com>
2020-11-30 10:43:26 -0300

    editar produto

--- src/app/produto/produto.page.ts
+++ src/app/produto/produto.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+editimport { FormGroup, FormBuilder } from "@angular/forms";
+import { DbService } from './../services/db.service';
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: 'app-produto',
@@ -6,10 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./produto.page.scss'],
 })
 export class ProdutoPage implements OnInit {
+  editForm: FormGroup;
+  id: any;
+  constructor(    
+    private db: DbService,
+    private router: Router,
+    public formBuilder: FormBuilder,
+    private actRoute: ActivatedRoute
+  ) { 
+    this.id = this.actRoute.snapshot.paramMap.get('id');
 
-  constructor() { }
+    this.db.getProduto(this.id).then(res => {
+      this.editForm.setValue({
+        produto_name: res['produto_name'],
+        unidadeId: res['unidadeId'],
+        categoriaId: res['categoriaId'],
+        quantidade: res['quantidade']
+
+      })
+    })
+  }
 
   ngOnInit() {
+    this.editForm = this.formBuilder.group({
+      produto_name: [''],
+      unidadeId: [''],
+      categoriaId: [''],
+      quantidade: ['']
+  })
+}
+saveForm(){
+    this.db.updateProduto(this.id, this.editForm.value)
+    .then( (res) => {
+      console.log(res)
+      this.router.navigate(['/produtos']);
+    })
   }
 
 }
